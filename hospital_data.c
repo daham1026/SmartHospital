9a7a6219f59e2294b9c514d45df00020b2289b2f
daham1026 <dahamuniv@gmail.com>
2026-09-18 18:13:30 +0530

    add doctor speciality and ward data

--- hospital_data.c
+++ hospital_data.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
 #include "hospital_data.h"
 
+const char specialtyNames[4][30] = {"General Practice","Paediatrics","Cardiology","Neurology"};
+const float specialtyFees[4] = {1500.00,2500.00,4500.00,5000.00};
+const int specialtyTimes[4] = {15,20,30,30};
+const int specialtyCaps[4] = {30,20,12,10};
+const char wardNames[4][30] = {"General Ward","Paediatric Ward","Surgical Ward","ICU"};
+const float wardCosts[4] = {3000.00,6000.00,12000.00,25000.00};
+const int wardBeds[4] = {20,10,10,5};
+
 void displayHospitalInformation()
 {
-    printf("Hospital information module\n");
+    int i;
+
+    printf("\n--- Doctor Specialties ---\n");
+    printf("\n");
+    printf("____________________________________________________________________________\n");
+    printf("ID  |   SPECIALITY     |     FEE     | CONSULTATION TIME | DAILY PATIENT CAP\n");
+    printf("----------------------------------------------------------------------------\n");
+
+    for (i = 0; i < 4; i++)
+    {
+        printf(" %-3d|%-17s | LKR %.2f | %8d minutes  |%8d patients\n",
+               i + 1,
+               specialtyNames[i],
+               specialtyFees[i],
+               specialtyTimes[i],
+               specialtyCaps[i]);
+    }
+    printf("----------------------------------------------------------------------------\n");
+    printf("\n--- Hospital Wards ---\n");
+    printf("\n");
+    printf("___________________________________________________________\n");
+    printf("ID  |   SPECIALITY     |      FEE         | NUMBER OF BEDS \n");
+    printf("-----------------------------------------------------------\n");
+
+
+
+    for (i = 0; i < 4; i++)
+    {
+        printf(" %-2d | %-16s | LKR %8.2f p.d | %4d beds\n",
+               i + 1,
+               wardNames[i],
+               wardCosts[i],
+               wardBeds[i]);
+    }
+     printf("-----------------------------------------------------------\n");
 }
--- hospital_data.h
+++ hospital_data.h
@@ -1,6 +1,15 @@
 #ifndef HOSPITAL_DATA_H
 #define HOSPITAL_DATA_H
 
+extern const char specialtyNames[4][30];
+extern const float specialtyFees[4];
+extern const int specialtyTimes[4];
+extern const int specialtyCaps[4];
+
+extern const char wardNames[4][30];
+extern const float wardCosts[4];
+extern const int wardBeds[4];
+
 void displayHospitalInformation();
 
 #endif
